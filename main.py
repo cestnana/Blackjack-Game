82d0f9d70b81634b989e72df4b2c95b63913d9d1
Remi Sha <105662433+cestnana@users.noreply.github.com>
2024-10-05 04:28:51 +0800

    Re-Organized PlayersTurn Logic (worked?)

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 # dealer_cards = []
 player_score = 0
 dealer_score = 0
+# flag use to determine if dealer need to proceed
+is_player_burst = False
 
 def calculate_score(cards):
   score = 0
@@ -41,33 +43,49 @@ def final_result(result_message):
   print(f"Dealer's final hand: {dealer_cards}, final score: {calculate_score(dealer_cards)}")
   print(f"{result_message}")
   
-def players_turn(player_cards, player_score, is_player_hits):
-  while player_score <= 21 and is_player_hits == "y":
-    # player get another card
-    player_cards.append(random.choice(deck))
-    # sum player scores
-    player_score = calculate_score(player_cards)
-    # check if Ace in hands before show current score
-    if 11 in player_cards:
-      # treat Ace as 1
-      replace_11_with_1(player_cards)
-    # show current result
-    current_result()
-    if player_score > 21:
-      print("You burst!")
-      result_message = "You lose."
-      final_result(result_message)
-      break
+def players_turn(player_cards, player_score, is_player_burst):
+  # is_player_hits = "y"
+  while player_score <= 21:
     # ask if player want to get another card
     is_player_hits = input("Type 'y' to get another card, type 'n' to pass: ")
-  return is_player_hits
+    if is_player_hits == "y":
+    # # flag use to determine if dealer need to proceed
+    # is_player_burst = False
+    
+      # player get another card
+      player_cards.append(random.choice(deck))
+      # sum player scores
+      player_score = calculate_score(player_cards)
+      # check if Ace in hands before show current score
+      if player_score > 21 and 11 in player_cards:
+        # treat Ace as 1
+        replace_11_with_1(player_cards)
+      # show current result
+      player_score = calculate_score(player_cards)
+      current_result()
+      if player_score > 21:
+        is_player_burst = True
+        print("You burst!")
+        result_message = "You lose."
+        final_result(result_message)
+        return is_player_burst
+    # return is_player_burst
+    elif is_player_hits == "n":
+      break
     
 def dealers_turn(dealer_cards, dealer_score):
-  while dealer_score <= 17:
+  while dealer_score < 17:
     # dealer get another card
     dealer_cards.append(random.choice(deck))
     # sum dealer scores
     dealer_score = calculate_score(dealer_cards)
+    # check if Ace in hands before show current score
+    if dealer_score > 21 and 11 in dealer_cards:
+      # treat Ace as 1
+      replace_11_with_1(dealer_cards)
+    # show current result
+    dealer_score = calculate_score(dealer_cards)
+    
     # check if dealer burst
     if dealer_score > 21:
       print("Dealer burst!")
@@ -96,6 +114,7 @@ while is_game_continue:
   print("\n")
   is_game_continue = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
   if is_game_continue == "y":
+    is_player_burst = False
     # initial first hands of both
     player_cards = []
     dealer_cards = []
@@ -104,20 +123,21 @@ while is_game_continue:
     player_score = calculate_score(player_cards)
     dealer_score = calculate_score(dealer_cards)
     current_result()
-    # ask user input "y" for hits, "n" for stands
-    is_player_hits = input("Type 'y' to get another card, type 'n' to pass: ")
-    if is_player_hits == "y":
-      # It's players turn
-      # players turn -> invoke function
-      players_turn(player_cards=player_cards, player_score=player_score, is_player_hits=is_player_hits)
-      
-    # if is_player_hits == "n":
+    # It's players turn
+    # players turn -> invoke function
+    players_turn(player_cards=player_cards, player_score=player_score, is_player_burst=is_player_burst)
+    
+  # if is_player_hits == "n":
+    if is_player_burst == False:
       # It's dealer's turn
       # dealer's turn -> invoke function
       dealers_turn(dealer_cards=dealer_cards, dealer_score=dealer_score)
-    
+  
     if calculate_score(player_cards) <= 21 and calculate_score(dealer_cards) <= 21:
       determine_result()
-    
-  else:
+  elif is_game_continue == "n":
     break
+  else:
+    print("Something went wrong!")
+    
+    
