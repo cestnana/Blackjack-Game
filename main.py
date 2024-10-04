bb27fddd5453e670e8ba1ef6480c555af204438e
Remi Sha <105662433+cestnana@users.noreply.github.com>
2024-10-05 00:39:16 +0800

    main game logic complete, separate func out.

--- main.py
+++ main.py
@@ -1,80 +1,112 @@
 import random
-# import art
-# print(art.logo)
+import art
+# clear the screen
+print("\n" * 20)
+# show the logo
+print(art.logo)
 
 # initialize card deck
 deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-player_cards = []
-dealer_cards = []
+# player_cards = []
+# dealer_cards = []
 player_score = 0
 dealer_score = 0
-  
-def initial_hands(player_score, dealer_score):
-    # randomly deal card for player
-    player_cards.append(random.choice(deck))
-    player_cards.append(random.choice(deck))
-    # randomly deal card for dealer
-    dealer_cards.append(random.choice(deck))
-    dealer_cards.append(random.choice(deck))
 
-    for score in player_cards:
-      player_score += score
-    for score in dealer_cards:
-      dealer_score += score
-    # return player_score, dealer_score
+def calculate_score(cards):
+  score = 0
+  for card in cards:
+    score += card
+  return score
+  
+def initial_hands():
+  # Deal cards to both player and dealer
+  for _ in range(2):
+    player_cards.append(random.choice(deck))
+    dealer_cards.append(random.choice(deck))
 
 def current_result():
-  # show players all cards
-  # show players scores
-  print(f"Your cards: {player_cards}, current score: {player_score}")
+  # show players all cards and players scores
+  print(f"Your cards: {player_cards}, current score: {calculate_score(player_cards)}")
   # show dealer's first card
   print(f"Computer's first card: {dealer_cards[0]}")
   
 def final_result(result_message):
-  print(f"Your final hand: {player_cards}, final score: {player_score}")
-  print(f"Dealer's final hand: {dealer_cards}, final score: {dealer_score}")
+  print(f"Your final hand: {player_cards}, final score: {calculate_score(player_cards)}")
+  print(f"Dealer's final hand: {dealer_cards}, final score: {calculate_score(dealer_cards)}")
   print(f"{result_message}")
   
+def players_turn(player_cards, player_score, is_player_hits):
+  while player_score <= 21 and is_player_hits == "y":
+    # player get another card
+    player_cards.append(random.choice(deck))
+    # sum player scores
+    player_score = calculate_score(player_cards)
+    # show current result
+    current_result()
+    if player_score > 21:
+      print("You burst!")
+      result_message = "You lose."
+      final_result(result_message)
+      break
+    # ask if player want to get another card
+    is_player_hits = input("Type 'y' to get another card, type 'n' to pass: ")
+    
+def dealers_turn(dealer_cards, dealer_score):
+  while dealer_score <= 17:
+    # dealer get another card
+    dealer_cards.append(random.choice(deck))
+    # sum dealer scores
+    dealer_score = calculate_score(dealer_cards)
+    # check if dealer burst
+    if dealer_score > 21:
+      print("Dealer burst!")
+      result_message = "You wins!"
+      final_result(result_message)
+
+def determine_result():
+  player_score = calculate_score(player_cards)
+  dealer_score = calculate_score(dealer_cards)
+  if player_score == dealer_score:
+    result_message = "It's draw."
+    final_result(result_message)
+  elif player_score > dealer_score: 
+    result_message = "You wins!"
+    final_result(result_message)
+  elif player_score < dealer_score:
+    result_message = "You lose."
+    final_result(result_message)
+
 is_game_continue = "y"
 result_message = "Game is not set."
 
+# game main logic here
 while is_game_continue: 
+  # clear the screen
+  print("\n")
   is_game_continue = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
-  while is_game_continue == "y":
-    initial_hands(player_score=player_score, dealer_score=dealer_score)
+  if is_game_continue == "y":
+    # initial first hands of both
+    player_cards = []
+    dealer_cards = []
+    initial_hands()
+    # update both player and dealer's score based on cards get
+    player_score = calculate_score(player_cards)
+    dealer_score = calculate_score(dealer_cards)
     current_result()
     # ask user input "y" for hits, "n" for stands
     is_player_hits = input("Type 'y' to get another card, type 'n' to pass: ")
     if is_player_hits == "y":
       # It's players turn
-      # player get another card
-      player_cards.append(random.choice(deck))
-      # sum player scores
-      for score in player_cards:
-        player_score + score
-      if player_score <= 21:
-        if player_score == dealer_score:
-          result_message = "It's draw."
-          final_result(result_message)
-        elif player_score > dealer_score: 
-          result_message = "You wins!"
-          final_result(result_message)
-      else:
-        result_message = "You lose."
-        final_result(result_message)
-    else:
+      # players turn -> invoke function
+      players_turn(player_cards=player_cards, player_score=player_score, is_player_hits=is_player_hits)
+      
+    elif is_player_hits == "n":
       # It's dealer's turn
-      dealer_cards.append(random.choice(deck))
-      # sum dealer scores
-      for score in dealer_cards:
-        dealer_score + score
-      if dealer_score <= 21:
-        if dealer_score == dealer_score:
-          result_message = "It's draw."
-          final_result(result_message)
-        elif dealer_score > player_score: 
-          result_message = "Dealer wins!"
-          final_result(result_message)
-      else:
-        result_message = "Player win."
-        final_result(result_message)
+      # dealer's turn -> invoke function
+      dealers_turn(dealer_cards=dealer_cards, dealer_score=dealer_score)
+    
+    if calculate_score(player_cards) <= 21 and calculate_score(dealer_cards) <= 21:
+      determine_result()
+    
+  else:
+    break
