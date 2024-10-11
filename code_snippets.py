980a751b0760198a0da3db6fd40b28d24080128e
Remi Sha <105662433+cestnana@users.noreply.github.com>
2024-10-12 03:40:27 +0800

    update code snippets

--- code_snippets.py
+++ code_snippets.py
@@ -3,3 +3,7 @@
 # \n Type 'y' for Yes or 'n' for No: 
 
 # is_game_continue = input("Do you want to play a game of Blackjack?\n Please Type 'y' for Yes or 'n' for No: ")
+
+# print("Something went wrong!")
+
+# print("Please enter 'y' or 'n' for further actions.")
